25a3d84b069b3a9940ce2afcaa76a448ea5b1a68
thiagoasb <thiagoasbezerra@gmail.com>
2021-04-02 17:10:30 -0300

    Adding authentication check

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,60 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        // verifica se usuário existe
+        if (!user) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        // verifica se a senha está correta
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        // gerar o jsonwebtoken
+        const token = sign({}, "9798419ca1a570c7337ce85c71b7436c", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
