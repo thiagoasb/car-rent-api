68698b1f2169ebdfc8ca0c1ca743cd788e79b464
thiagoasb <thiagoasbezerra@gmail.com>
2021-05-08 18:28:48 -0300

    Database connection file changed and seed to admin created

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,11 +6,11 @@ import swaggerUi from "swagger-ui-express";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 
-import "@shared/infra/typeorm";
-
+createConnection();
 const app = express();
 
 app.use(express.json());
