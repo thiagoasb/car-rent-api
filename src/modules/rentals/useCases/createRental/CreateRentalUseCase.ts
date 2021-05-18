bca7fe4a433a4ec0be8c5ab1784c230a7b26adc0
thiagoasb <thiagoasbezerra@gmail.com>
2021-05-18 17:08:17 -0300

    (feat) created rental controller, routes and providers/finished useCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,4 +1,8 @@
+import { inject, injectable } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/infra/typeorm/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -7,14 +11,23 @@ interface IRequest {
     expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
-    constructor(private rentalsRepository: IRentalsRepository) {}
+    constructor(
+        @inject("RentalsRepository")
+        private rentalsRepository: IRentalsRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
+    ) {}
 
     async execute({
         user_id,
         car_id,
         expected_return_date,
-    }: IRequest): Promise<void> {
+    }: IRequest): Promise<Rental> {
+        const minimumHour = 24;
+
+        // Não deve ser possível cadastrar um novo aluguel caso já exista um aluguel aberto para o mesmo carro
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
             car_id
         );
@@ -23,6 +36,7 @@ class CreateRentalUseCase {
             throw new AppError("Car is unavailable");
         }
 
+        // Não deve ser possível cadastrar um novo aluguel caso já exista um aluguel aberto para o mesmo usuário
         const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
             user_id
         );
@@ -30,6 +44,26 @@ class CreateRentalUseCase {
         if (rentalOpenToUser) {
             throw new AppError("There's a rental in progress for user!");
         }
+
+        // O aluguel deve ter duração mínima de 24 horas
+        const dateNow = this.dateProvider.dateNow();
+
+        const compare = this.dateProvider.compareInHours(
+            dateNow,
+            expected_return_date
+        );
+
+        if (compare < minimumHour) {
+            throw new AppError("Invalid return time!");
+        }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+            car_id,
+            expected_return_date,
+        });
+
+        return rental;
     }
 }
 
